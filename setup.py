27bd37d1c1f2d7a575f5988dac9530b2f37f2261
Xinyang Li <xinyang.li@tum.de>
2023-09-01 00:08:47 +0200

    change version to 0.1.0

--- setup.py
+++ setup.py
@@ -42,6 +42,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/xinyanglii/isac",
-    version="1.0.1",
+    version="0.1.0",
     zip_safe=False,
 )
